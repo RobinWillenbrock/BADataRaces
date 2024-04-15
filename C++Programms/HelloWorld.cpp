46bd37cab4eea893f10ebf83a6b896c18998c16a
Robin Willenbrock <r.willenbrock@web.de>
2024-04-15 17:15:52 +0200

    new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	modified:   C++Programms/HelloWorld.cpp 	modified:   C++Programms/HelloWorld.exe 	new file:   C++Programms/a-HelloWorld.cpp.015t.cfg 	modified:   Latex/main.fdb_latexmk 	modified:   Latex/main.fls 	modified:   Latex/main.log 	modified:   Latex/main.pdf 	modified:   Latex/main.synctex.gz 	modified:   Latex/main.tex

--- C++Programms/HelloWorld.cpp
+++ C++Programms/HelloWorld.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <time.h>
 int main() {
     const char* a = "HelloWorld"; 
-    const char* b = "HelloTUHH";  
+    const char* b = "HelloTUHH"; 
+    srand(time(NULL));
+    int count = 0;
+    int random;
 
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
 
-    a = "ByeWorld"; 
-    std::cout << a << std::endl;
 
-    return 0;
+while(count < 5){
+    count = count +1;
+    random = rand() % 10;
+    std::cout << random << std::endl;
+    if (random % 2 == 0)
+        std::cout << a << std::endl;
+    else
+        std::cout << b << std::endl;
+}
+    if (random % 2 == 0){
+        a = "ByeWorld"; 
+        std::cout << a << std::endl;
+    }else
+    {
+        a = "ByeTUHH";
+        std::cout << a << std::endl;
+    }
+return 0;
 }
