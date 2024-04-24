9a711685081587e91547545a3e0e23defad0a577
Robin Willenbrock <r.willenbrock@web.de>
2024-04-24 09:51:07 +0200

    modified:   C++Programms/parser.py

--- C++Programms/parser.py
+++ C++Programms/parser.py
@@ -9,17 +9,27 @@ def parse_cfg_data(cfg_file):
             lines = file.readlines()
             for line in lines:
                 line = line.strip()
+                print("Line:", line)  # Debug
                 match = re.match(r'<bb (\d+)> :', line)
                 if match:
                     bb_id = int(match.group(1))  # Extracting basic block ID
+                    print("Basic block ID:", bb_id)  # Debug: Print the extracted ID
                     current_bb = {"id": bb_id, "successors": []}
                     basic_blocks[bb_id] = current_bb
                 elif line.startswith("  if"):
                     # Extracting successor basic blocks from if statements
                     successors = line.split("{")[1].split("}")[0].strip().split()
                     current_bb["successors"] = [int(succ.strip("<>")) for succ in successors]
-                elif line.startswith("goto"):
+                elif line.startswith(" goto"):
                     # Extracting successor basic blocks from goto statements
+                    successor_match = re.search(r'<bb (\d+)>', line)
+                    if successor_match:
+                        successor = int(successor_match.group(1))
+                        current_bb["successors"].append(successor)
+                    else:
+                        print("Failed to extract successor ID from line:", line)
+                elif line.startswith("goto"):
+                    # Extracting successor basic blocks from goto statements without space
                     successor_match = re.search(r'<bb (\d+)>', line)
                     if successor_match:
                         successor = int(successor_match.group(1))
